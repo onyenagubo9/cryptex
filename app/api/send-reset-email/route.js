6e6ae2290cc61e5806ed16abf8dcf63a0da4d5cc
obinna <you@onyenaguboanthony9@gmail.com>
2025-10-17 16:49:28 +0100

    Updated icons and email templates

--- app/api/send-reset-email/route.js
+++ app/api/send-reset-email/route.js
@@ -1,14 +1,16 @@
-// app/api/send-reset-email/route.ts
 import { NextResponse } from "next/server";
 import { sendPasswordResetEmail } from "firebase/auth";
 import { auth } from "@/firebase/config";
 
-export async function POST(req: Request) {
+export async function POST(req) {
   try {
     const { email } = await req.json();
 
     if (!email) {
-      return NextResponse.json({ ok: false, error: "Email is required" }, { status: 400 });
+      return NextResponse.json(
+        { ok: false, error: "Email is required" },
+        { status: 400 }
+      );
     }
 
     await sendPasswordResetEmail(auth, email, {
@@ -16,9 +18,15 @@ export async function POST(req: Request) {
       handleCodeInApp: true,
     });
 
-    return NextResponse.json({ ok: true, message: "Reset email sent! Check your inbox or spam" });
-  } catch (err: any) {
+    return NextResponse.json({
+      ok: true,
+      message: "Reset email sent! Check your inbox or spam",
+    });
+  } catch (err) {
     console.error("Error sending reset email:", err.message);
-    return NextResponse.json({ ok: false, error: err.message }, { status: 500 });
+    return NextResponse.json(
+      { ok: false, error: err.message },
+      { status: 500 }
+    );
   }
 }
